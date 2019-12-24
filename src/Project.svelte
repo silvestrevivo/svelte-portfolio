b986d2a2e13f2ca67f2982bc984b5f3e02383389
silvestrevivo <silvestrevivomillan@gmail.com>
2019-12-24 11:25:29 +0100

    first approach for layout

---
+++ src/Project.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let name;
+  export let url;
+</script>
+
+<style>
+  li {
+    list-style-type: none;
+    font-size: 15px;
+    margin-bottom: 10px;
+    padding: 8px 20px;
+    background-color: #323330;
+    color: azure;
+    text-transform: uppercase;
+  }
+  @media only screen and (min-width: 600px) {
+    li {
+      font-size: 20px;
+    }
+  }
+</style>
+
+<li>
+  <a href={url} target="_blank">{name}</a>
+</li>
