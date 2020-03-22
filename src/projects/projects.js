0fe69359bb848ac6b6c89b9f9cd9b466eca5cb10
silvestrevivo <silvestrevivomillan@gmail.com>
2020-03-22 23:13:33 +0100

    svelte-marquee added

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -18,5 +18,10 @@ export default [
     id: Math.random() * Date.now(),
     name: "testing-library meetup",
     url: 'https://github.com/silvestrevivo/svelte-testing-library-meetup'
+  },
+  {
+    id: Math.random() * Date.now(),
+    name: "npm svelte-marquee",
+    url: 'https://www.npmjs.com/package/svelte-marquee'
   }
 ];
